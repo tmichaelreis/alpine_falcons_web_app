97ad0e9523cf3d20d0cc0e05a517efab301d87f4
Tom Reis <tom@commercekitchen.com>
2017-11-01 00:10:01 -0600

    [FEAT] Configure production environment for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,13 @@ Rails.application.configure do
   config.active_job.queue_name_prefix = "alpine_falcons_web_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+ # Email settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'http://myapp.herokuapp.com' }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -94,7 +98,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'alpinefalcons.com',
+    :domain => 'alpine-falcons.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
